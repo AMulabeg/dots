007515d993b88e9038dd1d73715e4bab6063dcb0
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-07-27 14:48:39 +0200

    Changes

--- .config/sketchybarMAIN/items/vpn.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-sketchybar -m --add item vpn right \
-              --set vpn icon=󰖂\
-                        update_freq=5 \
-                        script="~/.config/sketchybar/plugins/vpn.sh"
--- .config/sketchybarMAIN/plugins/clock.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-LABEL=$(date '+%H:%M:%S')
-sketchybar --set "$NAME" label="$LABEL"
