f69b8532d8c0741d473c1ad706bd4aac99c83b90
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-07-28 10:21:04 +0200

    Revamping Dots

--- .config/sketchybar/plugins/volume.sh
+++ .config/sketchybar/plugins/volume.sh
@@ -1,20 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# The volume_change event supplies a $INFO variable in which the current volume
-# percentage is passed to the script.
+# Get current volume level
+volume=$(osascript -e 'output volume of (get volume settings)')
+case $volume in
+  9[0-9]|100) ICON="􀊩"
+    PADDING_LEFT=3
+    PADDING_RIGHT=6
+  ;;
+  [6-8][0-9]) ICON="􀊧"
+    PADDING_LEFT=5
+    PADDING_RIGHT=7
+  ;;
+  [3-5][0-9]) ICON="􀊥"
+    PADDING_LEFT=6
+    PADDING_RIGHT=10
+  ;;
+  [1-2][0-9]) ICON="􀊡"
+    PADDING_LEFT=7
+    PADDING_RIGHT=14
+  ;;
+  *) ICON="􀊣"
+    PADDING_LEFT=7
+    PADDING_RIGHT=8
+esac
 
-if [ "$SENDER" = "volume_change" ]; then
-  VOLUME=$INFO
+# icon.padding_right=7\
+# icon.padding_left=8\
 
-  case $VOLUME in
-    [6-9][0-9]|100) ICON="􀊩"
-    ;;
-    [3-5][0-9]) ICON="􀊥"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="􀊡"
-    ;;
-    *) ICON="􀊣"
-  esac
-
-  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
-fi
+# Set label in sketchybar with volume value
+sketchybar --set volume_icon icon="$ICON"
+sketchybar --set volume_icon icon.padding_left="$PADDING_LEFT"
+sketchybar --set volume_icon icon.padding_right="$PADDING_RIGHT"
+sketchybar --set "$NAME" label="$volume%"
